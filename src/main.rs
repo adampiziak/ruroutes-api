320ab44b9099f1c2945c1cfb9b455d5b48c6567b
adam-piziak <adpiziak@gmail.com>
2018-01-06 12:55:26 -0500

    test with NEXT API

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,20 @@ extern crate reqwest;
 use std::io::Read;
 
 #[get("/")]
-fn index() -> &'static str {
-   " rutgers link"
+fn index() -> String {
+    print_route_config()
 }
 
 fn main() {
-    let mut resp = reqwest::get("https://www.rust-lang.org").unwrap();
+    rocket::ignite().mount("/", routes![index]).launch();
+}
+
+fn print_route_config() -> String {
+     let mut resp = reqwest::get("http://webservices.nextbus.com/service/publicJSONFeed?command=routeConfi
+g&a=rutgers&r=h").unwrap();
     assert!(resp.status().is_success());
 
     let mut content = String::new();
     resp.read_to_string(&mut content);
-    println!("{}", content);
-    rocket::ignite().mount("/", routes![index]).launch();
-}
-
-fn get_routes() -> Vec<String> {
-    vec![]
+    content
 }
