013b9641240907e9409b32d420e578cef0824411
adam-piziak <adpiziak@gmail.com>
2018-01-06 15:45:09 -0500

    deleted fn

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,10 @@ extern crate reqwest;
 use std::io::Read;
 
 #[get("/")]
-fn index() -> String {
-    print_route_config()
+fn index() -> &' str {
+    "root"
 }
 
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
-}
-
-fn print_route_config() -> String {
-     let mut resp = reqwest::get("http://webservices.nextbus.com/service/publicJSONFeed?command=routeConfi
-g&a=rutgers&r=h").unwrap();
-    assert!(resp.status().is_success());
-
-    let mut content = String::new();
-    resp.read_to_string(&mut content);
-    content
 }
