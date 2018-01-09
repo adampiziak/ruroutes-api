488f2f082b4cf352ef6e911f5681fe0d60c1a333
adam-piziak <adpiziak@gmail.com>
2018-01-09 12:40:48 -0500

    basic fetch from NEXT API and serve at /

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate reqwest;
 
 #[get("/api")]
 fn api() -> &'static str {
@@ -9,10 +10,18 @@ fn api() -> &'static str {
 }
 
 #[get("/")]
-fn index() -> &'static str {
-    "root"
+fn index() -> String {
+    get_config()
 }
 
 fn main() {
     rocket::ignite().mount("/", routes![index, api]).launch();
 }
+
+fn get_config() -> String {
+    let mut resp = reqwest::get("http://webservices.nextbus.com/service/publicJSONFeed?command=routeConfig&a=rutgers&terse");
+    
+    let body: String = resp.unwrap().text().unwrap();
+    body
+}
+
